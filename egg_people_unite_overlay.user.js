76b60d59166d2f6fb4e73dc4bc46232d622b9f02
Tonny2442 <48110960+Tonny2442@users.noreply.github.com>
2022-04-03 13:11:50 -0400

    Update egg_people_unite_overlay.user.js

--- egg_people_unite_overlay.user.js
+++ egg_people_unite_overlay.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         r/OMORI place template
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  r/omori template | OMORI WILL NOT SUCCUMB
+// @version      69
+// @description  OMORI WILL NOT SUCCUMB
 // @author       forked from a whole bunch of other people 
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/Tonny2442/omoriplace/raw/main/new_overlay.png";
+            i.src = "https://github.com/Tonny2442/omoriplace/raw/main/template1overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
