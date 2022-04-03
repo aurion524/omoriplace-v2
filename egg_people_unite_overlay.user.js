e40368497dcc2d665543b062b8dd3afbb62f19c0
Tonny2442 <48110960+Tonny2442@users.noreply.github.com>
2022-04-03 11:51:32 -0400

    Update egg_people_unite_overlay.user.js

--- egg_people_unite_overlay.user.js
+++ egg_people_unite_overlay.user.js
@@ -1,21 +1,22 @@
 // ==UserScript==
-// @name         April Fools r/RocketLeague Allies
+// @name         r/OMORI Place Overlay
 // @namespace    http://tampermonkey.net/
 // @version      3.7
 // @description  Keep the canvas beautiful!
-// @author       Adapted by u/kylomorales for r/RocketLeague, Original author oralekin from osu! /r/osuplace, Rocket League template by u/WestonHawk
+// @author       FORKED FROM R/ROCKETLEAGUE! | Adapted by u/kylomorales for r/RocketLeague, Original author oralekin from osu! /r/osuplace, Rocket League template by u/WestonHawk
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/kylomorales/rocketleagueplace/raw/main/egg_people_unite_overlay.user.js
-// @downloadURL  https://github.com/kylomorales/rocketleagueplace/raw/main/egg_people_unite_overlay.user.js
+// @updateURL    https://github.com/Tonny2442/omoriplace/raw/main/egg_people_unite_overlay.user.js
+// @downloadURL  https://github.com/Tonny2442/omoriplace/raw/main/egg_people_unite_overlay.user.js
 // @grant        none
+// OMORI WILL NOT SUCCUMB!
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/kylomorales/rocketleagueplace/raw/main/new_overlay.png";
+            i.src = "https://github.com/Tonny2442/omoriplace/raw/main/new_overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
